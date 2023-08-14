d095ea6abacd807d0827822a32d88063baf5bed9
TudorSante <tudorsantejudean.com>
2023-08-14 13:01:16 +0300

    Fetched the Genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,19 @@
+import { Text } from "@chakra-ui/react";
+import { useGenre } from "./hooks/useGenre";
+
+const GenreList = () => {
+  const { genres, error } = useGenre();
+
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GenreList;
